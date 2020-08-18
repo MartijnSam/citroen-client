e124e052dce59266c0f95fb7549e77385c60d24c
MartijnSam <martijnsamuels@gmail.com>
2020-08-18 22:29:41 +0200

    added content to aboutpage(over), responsive

--- src/pages/Over.js
+++ src/pages/Over.js
@@ -1,6 +1,66 @@
 import React from "react";
+import image1 from "../images/over/image1.jpg";
+import image2 from "../images/over/image2.jpg";
 import "./Over.css";
 
 export default function Over() {
-  return <div className="over-pagina">HALLO OVER PAGINA</div>;
+  return (
+    <div className="over-page">
+      <div className="image-text">
+        <div className="textblock" id="text1">
+          <h2>OVER CITROEN CATERING</h2>
+          <p>
+            Rhoncus commodo, nulla. Fusce quis ipsum. Nulla neque massa, feugiat
+            sed, commodo in, adipiscing ut, est. In fermentum mattis ligula.
+            Nulla ipsum. Vestibulum condimentum condimentum augue. Nunc purus
+            risus, volutpat sagittis, lobortis at, dignissim sed, sapien. Fusce
+            porttitor iaculis ante. Curabitur eu arcu. Morbi quam purus, tempor
+            eget, ullamcorper feugiat, commodo ullamcorper, neque.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            libero leo, pellentesque ornare, adipiscing vitae, rhoncus commodo,
+            nulla. Fusce quis ipsum. Nulla neque massa, feugiat sed, commodo in,
+            adipiscing ut, est. In fermentum mattis ligula. Nulla ipsum.
+            Vestibulum condimentum condimentum
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            libero leo, pellentesque ornare, adipiscing vitae, rhoncus commodo,
+            nulla. Fusce quis ipsum. Nulla neque massa, feugiat sed, commodo in,
+            adipiscing ut, est. In fermentum mattis ligula. Nulla ipsum.
+            Vestibulum condimentum condimentum
+          </p>
+        </div>
+        <img id="img1" src={image1} alt="image1" />
+      </div>
+      <div className="image-text">
+        <img id="img2" src={image2} alt="image2" />
+        <div className="textblock" id="text2">
+          <p>
+            Rhoncus commodo, nulla. Fusce quis ipsum. Nulla neque massa, feugiat
+            sed, commodo in, adipiscing ut, est. In fermentum mattis ligula.
+            Nulla ipsum. Vestibulum condimentum condimentum augue. Nunc purus
+            risus, volutpat sagittis, lobortis at, dignissim sed, sapien. Fusce
+            porttitor iaculis ante. Curabitur eu arcu. Morbi quam purus, tempor
+            eget, ullamcorper feugiat, commodo ullamcorper, neque.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            libero leo, pellentesque ornare, adipiscing vitae, rhoncus commodo,
+            nulla. Fusce quis ipsum. Nulla neque massa, feugiat sed, commodo in,
+            adipiscing ut, est. In fermentum mattis ligula. Nulla ipsum.
+            Vestibulum condimentum condimentum
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            libero leo, pellentesque ornare, adipiscing vitae, rhoncus commodo,
+            nulla. Fusce quis ipsum. Nulla neque massa, feugiat sed, commodo in,
+            adipiscing ut, est. In fermentum mattis ligula. Nulla ipsum.
+            Vestibulum condimentum condimentum
+          </p>
+        </div>{" "}
+      </div>
+    </div>
+  );
 }
