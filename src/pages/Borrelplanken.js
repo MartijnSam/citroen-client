3d09f006f7d97d74918cb0c97417d5a2dfcc0467
MartijnSam <martijnsamuels@gmail.com>
2020-08-19 18:25:15 +0200

    made borrelplanken more responsive

--- src/pages/Borrelplanken.js
+++ src/pages/Borrelplanken.js
@@ -75,53 +75,59 @@ export default function Borrelplanken() {
               onClick={() => setShowPopup(false)}
               className="close-button"
             ></div>
-            <img
-              src={borrelData[showPopup].img}
-              alt={borrelData[showPopup].name}
-            />
-            <div className="borrel-popup-text">
-              <h3>BORRELPLANK {borrelData[showPopup].name.toUpperCase()}</h3>
-              <p>{borrelData[showPopup].descr}</p>
+            <div className="borrel-popup-content">
+              <div className="borrel-popup-image-container">
+                <img
+                  src={borrelData[showPopup].img}
+                  alt={borrelData[showPopup].name}
+                />
+              </div>
+              <div className="borrel-popup-text">
+                <h3>BORRELPLANK {borrelData[showPopup].name.toUpperCase()}</h3>
+                <p>{borrelData[showPopup].descr}</p>
+              </div>
             </div>
           </div>
         </div>
       );
   }
-
+  const show = showPopup !== false ? "none" : "block";
   return (
     <div className="borrel-page">
       {borrelPopup()}
-      <div className="borrel-container">
-        {borrelData.map((borrel, i) => {
-          return (
-            <div
-              key={i}
-              className="borrel-card"
-              onClick={() => setShowPopup(i)}
-            >
-              <img src={borrel.img} alt={`borrelplank ${borrel.name}`} />
-              <h3>BORRELPLANK {borrel.name.toUpperCase()}</h3>
-              <ul>
-                {borrel.content.map((ing, i) => {
-                  return <li key={i}>{ing}</li>;
-                })}
-              </ul>
-            </div>
-          );
-        })}
-      </div>
-      <div className="text-block">
-        <h3>ONZE BORRELPLANKEN</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-          libero leo, pellentesque ornare, adipiscing vitae, rhoncus commodo,
-          nulla. Fusce quis ipsum. Nulla neque massa, feugiat sed, commodo in,
-          adipiscing ut, est. In fermentum mattis ligula. Nulla ipsum.
-          Vestibulum condimentum condimentum augue. Nunc purus risus, volutpat
-          sagittis, lobortis at, dignissim sed, sapien. Fusce porttitor iaculis
-          ante. Curabitur eu arcu. Morbi quam purus, tempor eget, ullamcorper
-          feugiat, commodo ullamcorper, neque.
-        </p>
+      <div className="borrel-page-content" style={{ display: show }}>
+        <div className="borrel-container">
+          {borrelData.map((borrel, i) => {
+            return (
+              <div
+                key={i}
+                className="borrel-card"
+                onClick={() => setShowPopup(i)}
+              >
+                <img src={borrel.img} alt={`borrelplank ${borrel.name}`} />
+                <h3>BORRELPLANK {borrel.name.toUpperCase()}</h3>
+                <ul>
+                  {borrel.content.map((ing, i) => {
+                    return <li key={i}>{ing}</li>;
+                  })}
+                </ul>
+              </div>
+            );
+          })}
+        </div>
+        <div className="text-block">
+          <h3>ONZE BORRELPLANKEN</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            libero leo, pellentesque ornare, adipiscing vitae, rhoncus commodo,
+            nulla. Fusce quis ipsum. Nulla neque massa, feugiat sed, commodo in,
+            adipiscing ut, est. In fermentum mattis ligula. Nulla ipsum.
+            Vestibulum condimentum condimentum augue. Nunc purus risus, volutpat
+            sagittis, lobortis at, dignissim sed, sapien. Fusce porttitor
+            iaculis ante. Curabitur eu arcu. Morbi quam purus, tempor eget,
+            ullamcorper feugiat, commodo ullamcorper, neque.
+          </p>
+        </div>
       </div>
     </div>
   );
